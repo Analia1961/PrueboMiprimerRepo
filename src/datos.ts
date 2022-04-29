6e20ce5c9c1c18d89ea3ca3b854bc5d240d8cec8
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-04-29 12:58:49 -0300

    ultima corrección ejercicio 1 Clase1 agregué datos en datos y en index para que se vea en console (29/4/2022)

--- src/datos.ts
+++ src/datos.ts
@@ -1,1 +1,12 @@
-let 
+ // ejercicio1 Clase 1
+console.log("crear una publicación en facebook");
+console.log("creo un álbum de fotos");
+console.log("elijo un título para el álbum");
+console.log("selecciono las fotos en mi PC");
+console.log("subo las fotos seleccionadas");
+console.log("agrego una descripción a cada foto");
+console.log("agrego colaboradores para que suban sus propias fotos");
+console.log("etiqueto algunos amigos");
+console.log("reviso destinatarios de la publicación");
+console.log("guardo y publico el álbum");
+console.log("me encanta");
